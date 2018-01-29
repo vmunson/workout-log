3123fc21480efd60b1cf26facce42ea3720dd1e2
vmunson <virginia.munson@gmail.com>
2018-01-29 09:01:19 -0500

    API Test

--- workoutlogServer/app.js
+++ workoutlogServer/app.js
@@ -1,6 +1,10 @@
 var express = require('express')
 var app = express()
 
+app.use('/api/test', function(req,res){
+    res.send('Hello World')
+})
+
 app.listen(3000, function(){
     console.log("app is open on 3000!")
 })
