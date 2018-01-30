00e56700bd2779a98ce526d54422e04b7bd4db1d
vmunson <virginia.munson@gmail.com>
2018-01-30 11:07:57 -0500

    added login

--- workoutlogClient/user/auth.js
+++ workoutlogClient/user/auth.js
@@ -31,12 +31,46 @@ $(function () {
                 .fail(function () {
                     $("#su_error").text("There was an issue with your username").show();
                 });
-        }
+        },
         //login method
-
+        login: function() {
+            console.log('login')
+             var username = $("#li_username").val();
+             var password = $("#li_password").val();
+             var user = {user:  {username: username, password: password }};
+             var login = $.ajax({
+             	type: "POST", 
+             	url: WorkoutLog.API_BASE + "login", 
+             	data: JSON.stringify(user), 
+             	contentType: "application/json"
+             });
+             login.done(function(data) {
+             	if (data.sessionToken) {
+                 WorkoutLog.setAuthHeader(data.sessionToken);
+             	}
+             		
+             	$("#login-modal").modal("hide");
+             	$(".disabled").removeClass("disabled");
+             	$("#loginout").text("Logout");
+             }) .fail(function() {
+             	$("#li_error").text("There was an issue with your username or password").show();
+        		});
+        },
         //loginout method
+        loginout: function(){
+            if(window.localStorage.getItem("sessionToken")){
+                window.localStorage.removeClass("sessionToken")
+                $("#loginout").text("Login")
+            }
+        }
     })
 
     //blind events
+    $("#loginButton").on("click", WorkoutLog.login)
     $("#signup").on("click", WorkoutLog.signup)
+    $("#loginout").on("click", WorkoutLog.loginout)
+
+    if(window.localStorage.getItem("sessionToken")){
+        $("#loginout").text("logout")
+    }
 })
